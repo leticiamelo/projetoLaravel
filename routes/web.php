ebaadac83eaf787f407e51ec7956566cd6c0c17c
Leticia Melo <erica.lety@hotmail.com>
2019-12-10 17:21:22 +0000

    boostrasp

--- routes/web.php
+++ routes/web.php
@@ -134,3 +134,7 @@ Route::get('/rotacomregras/{nome}/{n}', function ($nome, $n) {
   Route::get('opcoes/{opcao?}', function($opcao=null){
     return view('outras.opcoes', compact(['opcao']));
   })->name('opcoes');
+
+  Route::get('bootstrap', function(){
+    return view('outras.exemplo');
+  });
